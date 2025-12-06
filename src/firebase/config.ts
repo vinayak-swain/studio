e64bb7fec6f380732cec879399a49a427319fe0d
Vinayak Swain <vinayakswain49@gmail.com>
2025-12-06 18:23:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2480850685-9a424",
+  "appId": "1:712328307058:web:3b8949e25f4546f837fdeb",
+  "apiKey": "AIzaSyDcbwACZKeGvo_MIwPfl7JdMjZ38oKr_do",
+  "authDomain": "studio-2480850685-9a424.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "712328307058"
+};
